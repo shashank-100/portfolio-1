837456d84f5c8d000f1fde56534b9932837f32e7
Anil Kumar <anilk1sagar@gmail.com>
2024-02-19 20:32:35 +0530

    Updated footer added social links

--- src/app/pages/home/components/footer/footer.component.ts
+++ src/app/pages/home/components/footer/footer.component.ts
@@ -17,9 +17,18 @@ import {
 export class FooterComponent {
   currentYear = new Date().getFullYear();
   socials = [
-    { name: 'Linkedin', url: '', icon: faLinkedin, color: 'blue' },
-    { name: 'GitHub', url: '', icon: faGithub },
-    { name: 'StackOverflow', url: '', icon: faStackOverflow },
-    { name: 'Instagram', url: '', icon: faInstagram },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/anilk1sagar/',
+      icon: faLinkedin,
+      color: 'blue',
+    },
+    { name: 'GitHub', url: 'https://github.com/Anilk1sagar', icon: faGithub },
+    {
+      name: 'StackOverflow',
+      url: 'https://stackoverflow.com/users/8460199/anil-kumar',
+      icon: faStackOverflow,
+    },
+    { name: 'Instagram', url: 'https://www.instagram.com/anil_sagar_3/', icon: faInstagram },
   ];
 }
