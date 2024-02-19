75ecceefc75f7b2e89acf8321bbd98231a494d60
Anil Kumar <anilk1sagar@gmail.com>
2024-02-19 20:29:12 +0530

    Added footer

--- src/app/pages/home/components/footer/footer.component.ts
+++ src/app/pages/home/components/footer/footer.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faLinkedin,
+  faGithub,
+  faStackOverflow,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
+  socials = [
+    { name: 'Linkedin', url: '', icon: faLinkedin, color: 'blue' },
+    { name: 'GitHub', url: '', icon: faGithub },
+    { name: 'StackOverflow', url: '', icon: faStackOverflow },
+    { name: 'Instagram', url: '', icon: faInstagram },
+  ];
 }
